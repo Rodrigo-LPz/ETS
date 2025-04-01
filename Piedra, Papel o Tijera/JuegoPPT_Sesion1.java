e1d5db57fd6b886b271acf149cef5080219bbd5e
Rodrigo <135568662+Rodrigo-LPz@users.noreply.github.com>
2025-04-01 13:56:34 +0100

    Add files via upload

---
+++ Piedra, Papel o Tijera/JuegoPPT_Sesion1.java
@@ -0,0 +1,84 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class JuegoPPT_Sesion1{
+    // Método privado para la entrada de elección del usuario.
+    private static String usuario(Scanner usuario){
+        System.out.println("\nEscoga su arma de ataque:\n    1.'Piedra'.\n    2.'Papel'.\n    3.'Tijera'.\n    4.'Aleatorio'.");
+        String eleccion = usuario.nextLine().toLowerCase();
+        return eleccion;
+    }
+
+    // Método privado con elección aleatoria para la computadora.
+    private static String computadora(String[] opciones){
+        Random Aleatorio = new Random();
+        int eleccion = Aleatorio.nextInt(3); // opciones.length
+        return opciones[eleccion];
+    }
+
+    // Método para volver a jugar.
+    private static boolean jugarNuevamente(){
+        System.out.println("\n¿Desea continuar? (s/n)\n    Sí → s\n    No → n");
+        Scanner usuario = new Scanner(System.in);
+        String respuesta = usuario.nextLine().toLowerCase();
+        if (respuesta.equals("s")){
+            System.out.println("\nContinuemos!");
+            return true;
+        } else {
+            System.out.println("\nGracias por jugar!");
+            return false;
+        }
+    }
+
+    public static void main (String[] args){
+        // Introducción al programa.
+        Scanner usuario = new Scanner(System.in);
+        System.out.println("Bienvenido al programa 'Piedra, papel o tijera', por favor ingrese su nombre: ");
+        String nombre = usuario.nextLine();
+        System.out.println("\nBienvenido al programa " + nombre + " a continuación siga las instrucciones para jugar: ");
+
+        // Creación de las posibles opciones.
+        String[] opciones = {"Piedra", "Papel", "Tijera, Aleatorio"};
+        
+        // Creación de un objeto/"entidad", para leer la entrada del usuario
+        Scanner partida = new Scanner(System.in);
+        boolean jugar = true;
+
+        // Creación de un bucle de tipo "while" con el que leer los valores obtenidos de los métodos privados 'usuario', 'computadora' y 'jugarNuevamente' y parsear las distintas salidas posibles tras cotejar los resultados.
+        while (jugar == true){
+            String jugador = usuario(partida);
+            if (!jugador.equals("piedra") && !jugador.equals("papel") && !jugador.equals("tijera") && !jugador.equals("aleatorio")){
+                System.out.println("\nOpción no válida. Por favor, elija una opción válida: 'Piedra', 'Papel', 'Tijera' o 'Aleatorio'.");
+                jugador = usuario(partida);
+            }
+
+            if (jugador.equals("aleatorio")){
+                jugador = computadora(opciones);
+            }
+            String ordenador = computadora(opciones);
+
+            // Cotejamos los resultados.
+            System.out.println("\nJugador (tu elección): " + jugador);
+            System.out.println("\nOrdenador: " + ordenador);
+
+            // Determinamos los posibles caminos existentes para los resultados obtenidos.
+            String resultados;
+            if (jugador.equals(ordenador)){ /* También podemos ponerlo como "jugador == ordenador". */
+            resultados = "\nResultado final: Empate";
+            } else if ((jugador.equals("Piedra") && ordenador.equals("Tijera")) || (jugador.equals("Papel") && ordenador.equals("Piedra")) || (jugador.equals("Tijera") && ordenador.equals("Papel"))){
+                resultados = "\nResultado final: Ganaste";
+            } else{
+                resultados = "\nResultado final: Perdiste";
+            }
+            
+            // Muestreo de los resultados.
+            System.out.println(resultados);
+            
+            // Opción a volver a jugar.
+            jugar = jugarNuevamente();
+        }
+
+        // Cierre del scanner.
+        usuario.close();
+    }
+}
